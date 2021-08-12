35c3976a27a26c9e0f19288324d033369630f0a4
Bryan Kuo <bryan0011@gmail.com>
2021-08-12 14:25:01 +0800

    Initial Commit

---
+++ tippy01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tippy01
+//
+//  Created by Bryan Kuo on 2021/8/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
