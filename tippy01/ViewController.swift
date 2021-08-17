2fa70426e1b903a52b0f3062e4d2b98fda88e057
Bryan Kuo <bryan0011@gmail.com>
2021-08-18 00:55:59 +0800

    Final

--- tippy01/ViewController.swift
+++ tippy01/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountPriceText: UITextField!
+    @IBOutlet weak var tipPercentageText: UITextField!
+    @IBOutlet weak var peopleText: UITextField!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var averagePayLabel: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func finishTyping(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcuateAmount(_ sender: UITextField) {
+        let amountPrice = Double(amountPriceText.text!) ?? 0
+        let tipPercentage = Double(tipPercentageText.text!) ?? 0
+        let people = Double(peopleText.text!) ?? 0
+        
+        let tipsAmount = amountPrice * tipPercentage / 100
+        let totalAmount = tipsAmount + amountPrice
+        let payAmount = totalAmount / people
+        
+        tipAmountLabel.text = String(format: "$%.0f", tipsAmount)
+        totalAmountLabel.text = String(format: "$%.0f", totalAmount)
+        averagePayLabel.text = String(format: "$%.0f", payAmount)
+        
+        
+    }
+    
+    
 }
 
